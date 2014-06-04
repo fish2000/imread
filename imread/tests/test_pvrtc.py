8fa6d8a0e91e80aad4d20dd72c52453c384d0015
Alexander Böhn <fish2000@gmail.com>
2014-06-04 13:28:57 -0400

    Added assertions in PVR tests

--- imread/tests/test_pvrtc.py
+++ imread/tests/test_pvrtc.py
@@ -1,42 +1,54 @@
 from nose.tools import raises
+import numpy as np
 from imread import imread
 
 @raises(RuntimeError)
 def test_error():
     imread('./imread/tests/data/error.webp')
-
+    
 def test_AI88():
-    imread('./imread/tests/data/pvrsamples/AI88.pvr')
+    im = imread('./imread/tests/data/pvrsamples/AI88.pvr')
+    #assert np.any(im) FAIL WTF
 
 def test_I8():
-    imread('./imread/tests/data/pvrsamples/I8.pvr')
+    im = imread('./imread/tests/data/pvrsamples/I8.pvr')
+    assert np.any(im)
 
 def test_RGB565():
-    imread('./imread/tests/data/pvrsamples/RGB565.pvr')
+    im = imread('./imread/tests/data/pvrsamples/RGB565.pvr')
+    assert np.any(im)
 
 def test_RGB888():
-    imread('./imread/tests/data/pvrsamples/RGB888.pvr')
+    im = imread('./imread/tests/data/pvrsamples/RGB888.pvr')
+    assert np.any(im)
 
 def test_RGBA4444():
-    imread('./imread/tests/data/pvrsamples/RGBA4444.pvr')
+    im = imread('./imread/tests/data/pvrsamples/RGBA4444.pvr')
+    assert np.any(im)
 
 def test_RGBA5551():
-    imread('./imread/tests/data/pvrsamples/RGBA5551.pvr')
+    im = imread('./imread/tests/data/pvrsamples/RGBA5551.pvr')
+    assert np.any(im)
 
 def test_RGBA8888():
-    imread('./imread/tests/data/pvrsamples/RGBA8888.pvr')
+    im = imread('./imread/tests/data/pvrsamples/RGBA8888.pvr')
+    assert np.any(im)
 
 def test_apple_2bpp():
-    imread('./imread/tests/data/pvrsamples/apple_2bpp.pvr')
+    im = imread('./imread/tests/data/pvrsamples/apple_2bpp.pvr')
+    assert np.any(im)
 
 def test_apple_4bpp():
-    imread('./imread/tests/data/pvrsamples/apple_4bpp.pvr')
+    im = imread('./imread/tests/data/pvrsamples/apple_4bpp.pvr')
+    assert np.any(im)
 
 def test_pvrtc2bpp():
-    imread('./imread/tests/data/pvrsamples/pvrtc2bpp.pvr')
+    im = imread('./imread/tests/data/pvrsamples/pvrtc2bpp.pvr')
+    assert np.any(im)
 
 def test_pvrtc4bpp():
-    imread('./imread/tests/data/pvrsamples/pvrtc4bpp.pvr')
+    im = imread('./imread/tests/data/pvrsamples/pvrtc4bpp.pvr')
+    assert np.any(im)
 
 # pngreference.png ...?
 
